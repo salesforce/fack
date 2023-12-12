144954cdf78285d7e2d9431f7c4d84644b298d93
Vijay Swamidass <vijay.swamidass@gmail.com>
2023-12-12 11:10:20 -0800

    formatting

--- app/jobs/embed_document_job.rb
+++ app/jobs/embed_document_job.rb
@@ -1,15 +1,14 @@
 class EmbedDocumentJob < ApplicationJob
-    include SalesforceGptConcern  # Include the concern here
+  include SalesforceGptConcern  # Include the concern here
 
-    queue_as :default
-  
-    def perform(document_id)
-      document = Document.find(document_id)
-      begin
-        document.update(embedding: get_embedding(document.document))
-      rescue StandardError => e
-        Rails.logger.error("Error calling Salesforce Connect GPT.")
-      end
+  queue_as :default
+
+  def perform(document_id)
+    document = Document.find(document_id)
+    begin
+      document.update(embedding: get_embedding(document.document))
+    rescue StandardError => e
+      Rails.logger.error('Error calling Salesforce Connect GPT.')
     end
   end
-  
+end

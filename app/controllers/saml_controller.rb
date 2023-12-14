015379535b842159b7ea891c7dede6f8e06a6243
Vijay Swamidass <vijay.swamidass@gmail.com>
2023-12-14 14:41:17 -0800

    use saml env metadata url

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -2,7 +2,7 @@ class SamlController < ApplicationController
   skip_before_action :verify_authenticity_token
   def init
     request = OneLogin::RubySaml::Authrequest.new
-    redirect_to(request.create(saml_settings))
+    redirect_to(request.create(saml_settings), allow_other_host: true)
   end
 
   def consume
@@ -26,25 +26,12 @@ class SamlController < ApplicationController
   private
 
   def saml_settings
-    settings = OneLogin::RubySaml::Settings.new
+    idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+    # Returns OneLogin::RubySaml::Settings pre-populated with IdP metadata
+    settings = idp_metadata_parser.parse_remote(ENV['SSO_METADATA_URL'])
 
-    settings.assertion_consumer_service_url = "http://#{request.host}/saml/consume"
-    settings.sp_entity_id                   = "http://#{request.host}/saml/metadata"
-    settings.idp_sso_service_url = "https://app.onelogin.com/saml/signon/"
-    #settings.idp_cert_fingerprint           = OneLoginAppCertFingerPrint
-    settings.name_identifier_format         = 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
-
-    # Optional for most SAML IdPs
-    settings.authn_context = 'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport'
-
-    # Optional. Describe according to IdP specification (if supported) which attributes the SP desires to receive in SAMLResponse.
-    settings.attributes_index = 5
-    # Optional. Describe an attribute consuming service for support of additional attributes.
-    settings.attribute_consuming_service.configure do
-      service_name 'Service'
-      service_index 5
-      add_attribute name: 'Name', name_format: 'Name Format', friendly_name: 'Friendly Name'
-    end
+    settings.assertion_consumer_service_url = "http://#{request.host}/auth/saml/consume"
+    settings.sp_entity_id                   = "http://#{request.host}/auth/saml/metadata"
 
     settings
   end

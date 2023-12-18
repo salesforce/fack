85cc6efd5e7a18c877b0b773b54f0899f5679622
Vijay Swamidass <vijay.swamidass@gmail.com>
2023-12-18 14:51:13 -0800

    fix saml for login

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -5,15 +5,43 @@ class SamlController < ApplicationController
     redirect_to(request.create(saml_settings), allow_other_host: true)
   end
 
+  def lookup_or_create_user_by_email(email)
+    user = User.find_by_email(email)
+    return user if user
+
+    if user.nil?
+      # Create a user
+      user = User.new(email: email)
+    end
+
+    if user.save
+      puts "Created user: " + user.email
+    else
+      return nil  # Failed to create user
+    end
+
+    user
+  end
+
   def consume
     response = OneLogin::RubySaml::Response.new(params[:SAMLResponse])
     request = OneLogin::RubySaml::Authrequest.new
     response.settings = saml_settings
     if response.is_valid?
-      puts response.inspect
-      puts "EMAIL" + response.name_id
-      session[:authenticated] = true
-      redirect_to root_path
+      # Lookup user by email or create it
+      email = response.name_id
+
+      # TODO Add more error handling
+      user = lookup_or_create_user_by_email(email)
+
+      # Setting the session logs the user in.  Need to make some methods for this.
+      if user
+        session[:user_id] = user.id
+      else
+        notice = 'Login failed.  Please contact an admin for help.'
+      end
+      
+      redirect_to root_path, notice: notice
     else
       redirect_to(request.create(saml_settings))
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password validations: false
   validate :password_strength
 
   private

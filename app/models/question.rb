6f3e048d9649bd8c58e3ab16c7e1fec8af13570f
Vijay Swamidass <vijay.swamidass@gmail.com>
2023-12-19 11:23:22 -0800

    add option for slack markdown

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,27 @@
 class Question < ApplicationRecord
   has_neighbors :embedding
-  
+
   enum status: { pending: 0, generating: 1, generated: 2, failed: 3 }
 
   validates :question, presence: true
   belongs_to :user, optional: true
 
   before_save :check_unable_to_answer
+
+  def slack_markdown_answer
+    return if self.answer.nil?
+    # Regular expression to match Markdown links
+    # \[([^]]+)\] - Matches the link text inside square brackets
+    # \(http[^)]+\) - Matches the URL inside parentheses
+    markdown_regex = /\[([^\]]+)\]\((http[^)]+)\)/
+    
+    # Replace each Markdown link with Slack's format
+    self.answer.gsub(markdown_regex) do |_match|
+      link_text = ::Regexp.last_match(1)
+      url = ::Regexp.last_match(2)
+      "<#{url}|#{link_text}>"
+    end
+  end
 
   private
 
@@ -15,4 +30,6 @@ class Question < ApplicationRecord
 
     self.able_to_answer = false
   end
+
+
 end
--- app/views/api/v1/questions/_question.json.jbuilder
+++ app/views/api/v1/questions/_question.json.jbuilder
@@ -1,2 +1,3 @@
-json.extract! question, :id, :question, :answer, :created_at, :updated_at, :status, :able_to_answer
+json.extract! question, :id, :question, :answer, :created_at, :updated_at, :status, :able_to_answer,
+              :slack_markdown_answer
 json.url question_url(question)

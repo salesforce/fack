cf837c9dde6ad1803acab56298e2e72e763488c1
Vijay Swamidass <vijay.swamidass@gmail.com>
2024-02-07 15:08:19 -0800

    Enable ownership change

--- app/controllers/base_libraries_controller.rb
+++ app/controllers/base_libraries_controller.rb
@@ -62,6 +62,6 @@ class BaseLibrariesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def library_params
-    params.require(:library).permit(:name,:source_url)
+    params.require(:library).permit(:name, :source_url, :user_id)
   end
 end

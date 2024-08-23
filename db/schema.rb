e79dfec4fd551f3035360202625b0d91a9caf15d
vswamidass <vswamidass@salesforce.com>
2024-08-23 10:58:15 -0700

    add fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_23_003329) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_23_175752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -35,6 +35,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_23_003329) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.text "input"
+    t.text "output"
+    t.text "instructions"
+    t.text "context"
+    t.text "description"
   end
 
   create_table "chats", force: :cascade do |t|

86b8b919362b26480fe706a349ac0e3d241f891c
Vijay Swamidass <vswamidass@salesforce.com>
2024-09-03 13:38:36 -0700

    more tests

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   belongs_to :chat
   belongs_to :user
   after_create :enqueue_generate_message_response_job
+  validates :from, presence: true
 
   enum from: { user: 0, assistant: 1 }
   enum status: { ready: 0, generating: 1 }

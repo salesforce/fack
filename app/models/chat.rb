84079a59d17131960d2b941708a612ae5e38c3b9
Vijay Swamidass <vswamidass@salesforce.com>
2024-09-03 12:30:02 -0700

    make sure message content is there

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,4 +2,6 @@ class Chat < ApplicationRecord
   belongs_to :assistant
   belongs_to :user
   has_many :messages, dependent: :destroy
+  validates :first_message, presence: true
+  validates :assistant_id, presence: true
 end

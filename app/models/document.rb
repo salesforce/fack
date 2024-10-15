f83793f679e068d3c8d393294de72342eba3a5b5
Vijay Swamidass <vswamidass@salesforce.com>
2024-10-15 13:56:47 -0700

    Quip Sync For Docs (#76)
    * v1
    
    * basic sync
    
    * comments
    
    * x
    
    * ruby 3.2.5
    
    * version
    
    * v
    
    * v
    
    * quip status
    
    * unique
    
    * status
    
    * sync indicator

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Document < ApplicationRecord
   include PgSearch::Model
 
@@ -24,13 +22,28 @@ class Document < ApplicationRecord
   validate :token_count_must_be_less_than
 
   validates :external_id, uniqueness: true, if: -> { external_id.present? }
+  validates :source_url, uniqueness: true, if: -> { source_url.present? }
 
   validates :length, presence: true
 
   validates :document, presence: true,
-                       uniqueness: { scope: :check_hash, message: 'Document with same content already exists.' }
+                       uniqueness: { scope: :check_hash, message: 'Document with same content already exists.' },
+                       unless: -> { source_url.present? }
 
   before_validation :calculate_length, :calculate_tokens, :calculate_hash
+
+  after_save :sync_quip_doc_if_needed
+  after_commit :schedule_embed_document_job, if: -> { previous_changes.include?('check_hash') }
+
+  def source_type
+    if source_url.include?('quip.com')
+      'quip'
+    elsif source_url.present?
+      'other'
+    elsif source_url.blank?
+      'none'
+    end
+  end
 
   def calculate_length
     # Calculate the length of the 'document' column and store it in the 'length' column
@@ -69,5 +82,23 @@ class Document < ApplicationRecord
     return unless token_count.present? && token_count >= 8_000
 
     errors.add(:token_count, "is #{token_count} and must be less than 8,000")
+  end
+
+  # Sync the document with Quip if source_url is present, contains 'quip.com',
+  def sync_quip_doc_if_needed
+    return unless source_url.present? && source_url.include?('quip.com')
+
+    return unless synced_at.nil? # only schedule if it is for the initial sync
+
+    SyncQuipDocJob.perform_later(id)
+  end
+
+  # Schedule the EmbedDocumentJob with a delay based on the number of jobs in the queue
+  def schedule_embed_document_job
+    total_jobs = Delayed::Job.count
+    delay_seconds = total_jobs * 3 # 3-second delay per job in the queue
+
+    # Set the priority and delay, and queue the job if the check_hash has changed
+    EmbedDocumentJob.set(priority: 5, wait: delay_seconds.seconds).perform_later(id)
   end
 end

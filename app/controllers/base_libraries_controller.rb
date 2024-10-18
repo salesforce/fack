2acd7fdf43593d26bd1003f9d519e80cbc820b7e
vswamidass <vswamidass@salesforce.com>
2024-10-17 23:51:09 -0700

    perf improvement

--- app/controllers/base_libraries_controller.rb
+++ app/controllers/base_libraries_controller.rb
@@ -5,7 +5,7 @@ class BaseLibrariesController < ApplicationController
 
   # GET /libraries or /libraries.json
   def index
-    @libraries = Library.all.order(name: :asc).page(params[:page])
+    @libraries = Library.includes(:user).order(name: :asc).page(params[:page])
   end
 
   # POST /libraries or /libraries.json

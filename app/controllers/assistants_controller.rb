f024e481843760fe401005cef3a2ffecc046a956
Vijay Swamidass <vswamidass@salesforce.com>
2025-01-21 10:01:42 -0800

    Styling for Chats and Assistants.  (#106)
    * change nav
    
    * button style
    
    * nav
    
    * chat navigation and style
    
    * style list views.   Show recent chats on assistant.
    
    * sort
    
    * fix form

--- app/controllers/assistants_controller.rb
+++ app/controllers/assistants_controller.rb
@@ -2,7 +2,10 @@ class AssistantsController < BaseAssistantsController
   before_action :set_assistant, only: %i[show edit update destroy]
 
   # GET /assistants/1 or /assistants/1.json
-  def show; end
+  def show
+    @chat = Chat.new
+    @chat.assistant = Assistant.find(params[:id])
+  end
 
   # GET /assistants/new
   def new

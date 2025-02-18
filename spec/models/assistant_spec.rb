80c2126cd32be67a843c9d559623efc33079438d
Vijay Swamidass <vswamidass@salesforce.com>
2025-02-18 09:50:10 -0800

    assistant fixes (#127)
    * fix required fields.  Form tweak
    
    * show assistant details
    
    * tests

--- spec/models/assistant_spec.rb
+++ spec/models/assistant_spec.rb
@@ -4,12 +4,9 @@ RSpec.describe Assistant, type: :model do
   let(:user) { create(:user) }
 
   describe 'validations' do
-    it { should validate_presence_of(:libraries) }
-    it { should validate_presence_of(:input) }
-
     context 'libraries CSV validation' do
       it 'is valid with a valid CSV string of numbers' do
-        assistant = Assistant.new(libraries: '1,2,3.5,4', input: 'input', user:, instructions: 'instructions', output: 'output')
+        assistant = Assistant.new(name: 'test', libraries: '1,2,3.5,4', input: 'input', user:, instructions: 'instructions', output: 'output')
         expect(assistant).to be_valid
       end
 

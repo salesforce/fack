6bbb87c601cad6575eb9637b9fe0a0dfda09bbf7
Vijay Swamidass <vijay.swamidass@gmail.com>
2025-04-19 00:03:40 -0700

    fix recents

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,13 @@
 class DashboardController < ApplicationController
   def index
-    @recent_questions = Question.order(created_at: :desc).limit(5)
-    @recent_chats = Chat.order(created_at: :desc).limit(5)
+    @recent_questions = Question.where(user_id: current_user.id)
+                                .order(created_at: :desc)
+                                .limit(5)
+    @recent_chats = Chat.where(user_id: current_user.id)
+                        .order(created_at: :desc)
+                        .limit(5)
     @recent_assistants = Assistant.joins(:chats)
+                                  .where(chats: { user_id: current_user.id })
                                   .select('assistants.*, MAX(chats.created_at) as last_chat_time')
                                   .group('assistants.id')
                                   .order('last_chat_time DESC')

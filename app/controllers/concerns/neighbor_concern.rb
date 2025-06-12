53f868adb368fb4ad28f797bf2fdb5e3e1bed7b2
Vijay Swamidass <vijay.swamidass@gmail.com>
2025-06-12 14:32:15 -0700

    add doc vector search

--- app/controllers/concerns/neighbor_concern.rb
+++ app/controllers/concerns/neighbor_concern.rb
@@ -10,4 +10,10 @@ module NeighborConcern
   def related_documents_from_embedding(embedding)
     Document.nearest_neighbors(:embedding, embedding, distance: 'euclidean')
   end
+
+  def related_documents_from_embedding_by_libraries(_embedding, _library_ids)
+    scope = related_documents_from_embedding(_embedding)
+    scope = scope.where(library_id: _library_ids) if _library_ids.present?
+    scope.order(created_at: :desc).limit(100)
+  end
 end

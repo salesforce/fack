ea88311becfff4f5cf51a60bdbbcb09072037a2f
Vijay Swamidass <vijay.swamidass@gmail.com>
2025-07-10 16:48:05 -0700

    filter was being overwritten

--- app/controllers/concerns/neighbor_concern.rb
+++ app/controllers/concerns/neighbor_concern.rb
@@ -14,6 +14,6 @@ module NeighborConcern
   def related_documents_from_embedding_by_libraries(_embedding, _library_ids)
     scope = related_documents_from_embedding(_embedding)
     scope = scope.where(library_id: _library_ids) if _library_ids.present?
-    scope.order(created_at: :desc).limit(100)
+    scope.order(updated_at: :desc).limit(100)
   end
 end
